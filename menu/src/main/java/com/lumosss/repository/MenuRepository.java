2977afb52e2d532345cd21ce487e00fc1b223260
lumos <actyzhang@outlook.com>
2020-11-08 00:08:21 +0100

    first commit

---
+++ menu/src/main/java/com/lumosss/repository/MenuRepository.java
@@ -0,0 +1,15 @@
+package com.lumosss.repository;
+
+import com.lumosss.entity.Menu;
+
+import java.util.List;
+
+
+public interface MenuRepository {
+    public List<Menu> findAll(int index, int limit);
+    public int count();
+    public Menu findById(long id);
+    public void save(Menu menu);
+    public void update(Menu menu);
+    public void deleteById(long id);
+}
