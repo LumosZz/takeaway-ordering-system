4b9af31b24e8db66f1c919e302408b590181dfaa
lumosss <actyzhang@outlook.com>
2020-11-08 21:38:22 +0100

    feat: implement index.html

--- client/src/main/java/com/lumosss/controller/ClientHandler.java
+++ client/src/main/java/com/lumosss/controller/ClientHandler.java
@@ -1,24 +1,30 @@
 package com.lumosss.controller;
 
 import com.lumosss.entity.Menu;
+import com.lumosss.entity.MenuVO;
 import com.lumosss.feign.MenuFeign;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/client")
 public class ClientHandler {
 
     @Autowired
     private MenuFeign menuFeign;
 
-    @GetMapping("/findAll/{index}/{limit}")
-    public List<Menu> findAll(@PathVariable("index") int index, @PathVariable("limit") int limit) {
-        return menuFeign.findAll(index, limit);
+    @GetMapping("/findAll")
+    @ResponseBody
+    public MenuVO findAll(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+        int index = (page-1) * limit;
+        return new MenuVO(0, "", 100, menuFeign.findAll(index, limit));
+    }
+
+    @GetMapping("/redirect/{location}")
+    public String redirect(@PathVariable("location") String location) {
+        return location;
     }
 }
