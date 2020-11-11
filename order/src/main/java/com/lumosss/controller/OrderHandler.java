807ec34c70f32780023155e1fb3cfa654335b549
lumosss <actyzhang@outlook.com>
2020-11-11 19:10:12 +0100

    feat: implement order and deliver functionality

--- order/src/main/java/com/lumosss/controller/OrderHandler.java
+++ order/src/main/java/com/lumosss/controller/OrderHandler.java
@@ -1,19 +1,60 @@
 package com.lumosss.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lumosss.entity.Admin;
+import com.lumosss.entity.Order;
+import com.lumosss.entity.OrderVO;
+import com.lumosss.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/order")
 public class OrderHandler {
-    @Value("${server.port}")
-    private String port;
 
-    @GetMapping("/index")
-    public String index() {
-        return "Current port: " + this.port;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping("/save")
+    public void save(@RequestBody Order order){
+        order.setDate(new Date());
+        orderRepository.save(order);
+    }
+
+    @GetMapping("/findAllByUid/{uid}/{page}/{limit}")
+    public OrderVO findAllByUid(@PathVariable("uid") long uid, @PathVariable("page") int page, @PathVariable("limit") int limit){
+        OrderVO orderVO = new OrderVO();
+        orderVO.setCode(0);
+        orderVO.setMsg("");
+        orderVO.setCount(orderRepository.countByUid(uid));
+        orderVO.setData(orderRepository.findAllByUid(uid,(page-1)*limit,limit));
+        return orderVO;
+    }
+
+    @DeleteMapping("/deleteByMid/{mid}")
+    public void deleteByMid(@PathVariable("mid") long mid){
+        orderRepository.deleteByMid(mid);
+    }
+
+    @DeleteMapping("/deleteByUid/{uid}")
+    public void deleteByUid(@PathVariable("uid") long uid){
+        orderRepository.deleteByUid(uid);
+    }
+
+    @GetMapping("/findAllByState/{state}/{page}/{limit}")
+    public OrderVO findAllByState(@PathVariable("state") int state, @PathVariable("page") int page, @PathVariable("limit") int limit){
+        OrderVO orderVO = new OrderVO();
+        orderVO.setCode(0);
+        orderVO.setMsg("");
+        orderVO.setCount(orderRepository.countByState(0));
+        orderVO.setData(orderRepository.findAllByState(0,(page-1)*limit,limit));
+        return orderVO;
+    }
+
+    @PutMapping("/updateState/{id}/{state}/{aid}")
+    public void updateState(@PathVariable("id") long id, @PathVariable("state") int state, @PathVariable("aid") long aid){
+        orderRepository.updateState(id,aid,state);
     }
 
 }
